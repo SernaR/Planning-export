b1c15976ff290563e51715c99162868bcb4ee765
SernaR <rserna@outlook.fr>
2020-08-02 21:02:24 +0200

    env config

--- assets/js/services/config.js
+++ assets/js/services/config.js
@@ -1,10 +1,12 @@
-export const ORDERS_API = '/api/transport_orders'
-export const COUNTRY_API = '/api/destination_params'
-export const RATE_API = '/api/rates'
+export const API_URL = process.env.API_URL
+
+export const ORDERS_API = API_URL + 'transport_orders'
+//export const COUNTRY_API = API_URL + '/api/destination_params'
+export const RATE_API = API_URL + 'rates'
+export const CREATE_SETUP_API = API_URL + 'initial_params/1'
 
 export const ADMIN = 'https://localhost:8000/admin'
 export const LOGOUT ='https://localhost:8000/deconnexion'
 
-export const CREATE_SETUP_API = '/api/initial_params/1'
 
 export const itemsPerPage = 15
