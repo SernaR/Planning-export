7bad73171903146f691920803ac092ab212ebcd8
SernaR <rserna@outlook.fr>
2020-08-05 22:26:59 +0200

    fix api orderController

--- src/Controller/api/OrderController.php
+++ src/Controller/api/OrderController.php
@@ -5,19 +5,20 @@ namespace App\Controller\Api;
 use App\Entity\TransportOrder;
 use App\Repository\TransportOrderRepository;
 
-class OrderController
+class OrderController 
 {
-    private $code;
+    private $repo;
 
     public function __construct(TransportOrderRepository $transportOrderRepository)
     {
-        $this->code = $transportOrderRepository->lastOrderCode() + 1;
+        $this->repo = $transportOrderRepository;
     }
     
     public function __invoke(TransportOrder $data)
     {
+        $code = $this->repo->lastOrderCode() + 1;
         $country = $data->getFirstDeliveryWarehouse()->getAdress()->getCountry()->getCode();
-        $data->setCode($country.$this->code);
+        $data->setCode($country.$code);
         return $data;
     }
 }
